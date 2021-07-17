9d616ca32121147b3ec0b119dc0dca41dfbc9c89
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-17 17:40:11 +0900

    customerのCRUD機能をscaffoldで作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_083650) do
+ActiveRecord::Schema.define(version: 2021_07_17_083939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "customer_number"
+    t.string "customer_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "product_name"
