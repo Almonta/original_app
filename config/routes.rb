bbcc8add61147f61314478315921c1954d1b67b7
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-18 14:11:24 +0900

    navbarのactiveの有効設定をした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  # get 'homes/index'
+  post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
   resources :customers
   resources :products
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   get 'tops/index'
   root 'tops#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
