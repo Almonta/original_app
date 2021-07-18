92ec59b029bbe1f58bcfbcba947e7f399d40d6ed
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-18 18:33:10 +0900

    管理者のゲストログイン機能を作成した

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,4 +4,10 @@ class Users::SessionsController < Devise::SessionsController
     sign_in user
     redirect_to homes_index_path, notice: "#{user.name}ユーザー、所属「#{user.department}」としてログインしました。"
   end
+
+  def admin_guest_sign_in
+    admin_user = User.admin_guest
+    sign_in admin_user
+    redirect_to homes_index_path, notice: "#{admin_user.name}ユーザー、所属「#{admin_user.department}」としてログインしました。"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def self.guest
-    find_or_create_by!(name:'ゲスト', department: '未所属', email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'ゲスト', department: '未所属', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       # user.name = "ゲスト"
       # user.department = "未所属"
+      # user.admin = true
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
+    end
+  end
+
+  def self.admin_guest
+    find_or_create_by!(name: '管理者', department: '未所属', email: 'admin_guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.admin = true
     end
   end
 end
