3935573c265a370d5708febe7c90f7e1b9f564db
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-18 10:15:08 +0900

    usersテーブルにnameカラムの追加、入力フォームへ名前の入力欄を追加した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_142903) do
+ActiveRecord::Schema.define(version: 2021_07_18_004533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_142903) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.integer "department", default: 0, null: false
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
