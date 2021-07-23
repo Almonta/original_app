8d0b217409f7e99c5c6da0cebc590255d2d022f8
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-23 11:10:33 +0900

    customerに外部キーuser_idを追加した

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,10 +1,10 @@
 class UserProductsController < ApplicationController
   def create
     user_product = current_user.user_products.create(product_id: params[:product_id])
-    redirect_to homes_path, notice: "#{user_product.product.name}をお気に入り登録しました"
+    redirect_to homes_path, notice: "#{user_product.product.name}を「★登録プロジェクト」に登録しました"
   end
   def destroy
     user_product = current_user.user_products.find_by(id: params[:id]).destroy
-    redirect_to homes_path, notice: "#{user_product.product.name}をお気に入り解除しました"
+    redirect_to homes_path, notice: "#{user_product.product.name}を「★登録プロジェクト」から解除しました"
   end
 end
