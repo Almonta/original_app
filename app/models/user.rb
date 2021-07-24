118e3e5969fd37477a05f276ba6acbc607f96585
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-24 14:58:14 +0900

    所属のenum列挙型の登録をし、プロフィール画面に所属が表示されるようにした

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   def self.guest
-    find_or_create_by!(name: 'ゲスト', department: '未所属', email: 'guest@example.com') do |user|
+    find_or_create_by!(name: 'ゲスト', department: '0', email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
   end
 
   def self.admin_guest
-    find_or_create_by!(name: '管理者', department: '未所属', email: 'admin_guest@example.com') do |user|
+    find_or_create_by!(name: '管理者', department: '0', email: 'admin_guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.admin = true
     end
   end
 
-  enum departments: { 未所属: 0, 営業: 1, 技術: 2, 計画: 3 }
+  enum department: { 未所属: 0, 営業: 1, 技術: 2, 計画: 3 }
 
   has_many :products
   has_many :user_products, dependent: :destroy
