031aaf562099730d9973e28e77e1a4df66e9259a
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-25 17:58:37 +0900

    コメント投稿者表示リンクを修正した

--- app/controllers/product_contacts_controller.rb
+++ app/controllers/product_contacts_controller.rb
@@ -2,12 +2,13 @@ class ProductContactsController < ApplicationController
   before_action :set_product, only: %i[create edit update]
   def create
     # raise
-    # binding.irb
     # @product = Product.find(params[:product_id])
     @product_contact = @product.product_contacts.build(product_contact_params)
+    # binding.irb
     respond_to do |format|
       if @product_contact.save
         format.js { render :index }
+        # binding.irb
       else
         format.html { redirect_to product_path(@product), notice: '投稿できませんでした' }
       end
@@ -48,7 +49,7 @@ class ProductContactsController < ApplicationController
   private
 
   def product_contact_params
-    params.require(:product_contact).permit(:product_id, :contact_message).merge(user_id: current_user.id)
+    params.require(:product_contact).permit(:product_id, :user_id, :contact_message).merge(user_id: current_user.id)
   end
 
   def set_product
