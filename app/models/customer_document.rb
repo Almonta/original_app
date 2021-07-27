ae7cefdc9637882f735ae9cb34b43a0fda3839f5
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-27 22:45:36 +0900

    アップローダーを作成し、モデルに設定

--- app/models/customer_document.rb
+++ app/models/customer_document.rb
@@ -2,4 +2,5 @@ class CustomerDocument < ApplicationRecord
   validates :name, :content, :public_level, presence: true
   belongs_to :user
   belongs_to :customer
+  mount_uploader :document, DocumentUploader
 end
--- app/models/product_document.rb
+++ app/models/product_document.rb
@@ -2,4 +2,5 @@ class ProductDocument < ApplicationRecord
   validates :name, :content, :public_level, presence: true
   belongs_to :user
   belongs_to :product
+  mount_uploader :document, DocumentUploader
 end
