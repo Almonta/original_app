fd376e6aa3a1a4cd2a8218bdfdacf42015da03a4
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-27 16:48:00 +0900

    documentテーブルにmodelバリデーションを追加

--- app/models/customer_document.rb
+++ app/models/customer_document.rb
@@ -1,4 +1,5 @@
 class CustomerDocument < ApplicationRecord
+  validate :name, :content, :public_level, presence: true
   belongs_to :user
   belongs_to :customer
 end
--- app/models/product_document.rb
+++ app/models/product_document.rb
@@ -1,4 +1,5 @@
 class ProductDocument < ApplicationRecord
+  validate :name, :content, :public_level, presence: true
   belongs_to :user
   belongs_to :product
 end
