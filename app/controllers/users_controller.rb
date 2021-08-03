5cf8c5cc772d22bf2622e4762b9401f5f91f8f96
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-03 22:11:21 +0900

    ユーザーが登録したプロダクトとカスタマーを、プロフィール画面に表示されるようにした #42

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @products = Product.where(user_id: current_user.id)
+    @customers = Customer.where(user_id: current_user.id)
     # binding.irb
   end
 end
