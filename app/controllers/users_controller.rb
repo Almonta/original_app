2f23601dea5fb4919f61d45a9bbaab5687c968bc
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-03 22:36:28 +0900

    ユーザーが登録したドキュメントを、プロフィール画面に表示されるようにした #42

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
     @user = current_user
     @products = Product.where(user_id: current_user.id)
     @customers = Customer.where(user_id: current_user.id)
+    @product_documents = ProductDocument.where(user_id: current_user.id)
+    @customer_documents = CustomerDocument.where(user_id: current_user.id)
     # binding.irb
   end
 end
