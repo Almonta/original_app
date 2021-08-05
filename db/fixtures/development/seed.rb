b91ae224bf2dc1986fd51d7c9c288f97efb17b80
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-05 11:24:59 +0900

    seedデータ書き込み用ファイルとディレクトリを作成 #45

---
+++ db/fixtures/development/seed.rb
@@ -0,0 +1,10 @@
+10.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
