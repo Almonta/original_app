4c748f7056e5f1a3ad1e390ec0de9318d08f1de6
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-05 21:50:00 +0900

    プロダクト資料のseedデータを作成 #45

--- db/fixtures/development/seed.rb
+++ db/fixtures/development/seed.rb
@@ -58,16 +58,17 @@ UserCustomer.seed(
   { id: 10, user_id: 14, customer_id: 10 }
 )
 
-# ProductDocument.seed(
-#   :id,
-#   { id: 1,  name: ,  content: 1, document:  , public_level:   },
-#   { id: 2,  user_id: 6,  customer_id: 2  },
-#   { id: 3,  user_id: 7,  customer_id: 3  },
-#   { id: 4,  user_id: 8,  customer_id: 4  },
-#   { id: 5,  user_id: 9,  customer_id: 5  },
-#   { id: 6,  user_id: 10, customer_id: 6  },
-#   { id: 7,  user_id: 11, customer_id: 7  },
-#   { id: 8,  user_id: 12, customer_id: 8  },
-#   { id: 9,  user_id: 13, customer_id: 9  },
-#   { id: 10, user_id: 14, customer_id: 10 }
-# )
+
+content = 'アップ資料は参考です。スクールで学習したRubyのまとめ資料です。'
+ProductDocument.seed(
+  :id,
+  { id: 1,  user_id: 12, product_id: 1,  name: '組立手順書', content: "組立手順書です。#{content}", document:File.open("./db/documents/'21.07.07_チェリー本_7.2-7.3_クラスの定義.pdf"),        public_level: 0 },
+  { id: 2,  user_id: 8,  product_id: 1,  name: '取説',      content: "取説です。#{content}",      document:File.open("./db/documents/'21.07.07_チェリー本_7.4-7.6_クラスの継承.pdf"),        public_level: 0 },
+  { id: 3,  user_id: 8,  product_id: 1,  name: 'カタログ',   content: "カタログです。#{content}",   document:File.open("./db/documents/'21.07.07_チェリー本_7.7-7.8_メソッドの公開レベル.pdf"), public_level: 0 },
+  { id: 4,  user_id: 9,  product_id: 1,  name: '解析結果',   content: "解析結果です。#{content}",   document:File.open("./db/documents/'21.07.07_チェリー本_7.9-7.10_クラスの作成.pdf"),       public_level: 1 },
+  { id: 5,  user_id: 9,  product_id: 1,  name: '計算資料',   content: "計算資料です。#{content}",   document:File.open("./db/documents/'21.07.07_チェリー本_7.10_クラスの作成.pdf"),           public_level: 1 },
+  { id: 6,  user_id: 9,  product_id: 1,  name: '評価データ',  content: "評価データです。#{content}", document:File.open("./db/documents/'21.07.12_チェリー本_8.1-8.5_モジュールとは.pdf"),      public_level: 1 },
+  )
+
+
+  # { id: 3,  user_id: 6,  product_id: 1,  name: '注文書',     content: "注文書です。#{content}",    document:File.open("./db/documents/'21.07.07_チェリー本_7.7-7.8_メソッドの公開レベル.pdf"), public_level: 0 },
