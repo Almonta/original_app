70d01f0eac7bd99e2a376d29476150e3553dfe11
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-05 11:39:31 +0900

    Userのseedデータを作成した #45

--- db/fixtures/development/seed.rb
+++ db/fixtures/development/seed.rb
@@ -1,10 +1,13 @@
 10.times do |n|
   name = Faker::Name.name
   email = Faker::Internet.email
+  department = rand(1..5)
   password = "password"
   User.create!(name: name,
+               department: department,
                email: email,
                password: password,
                password_confirmation: password,
+               admin: false
                )
 end
