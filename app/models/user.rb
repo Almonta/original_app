ddab7ed4c8d460c64525c479255a1d2a84915f99
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-07 21:16:55 +0900

    userモデルのバリデーションにemailカラムをを追加設定 #55

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, length: { maximum: 255 }
+  validates :name, :email, presence: true, length: { maximum: 255 }
   validates :department, presence: true
 
   def self.guest
