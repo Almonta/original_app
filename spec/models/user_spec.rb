0ea4ecf8d07f68bc2e3d315be34ee0ebbac3b29c
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-08 15:25:46 +0900

    userモデルのnameの空欄、文字数テストを実施 #59

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    it '名前が正しく入力されていないときエラー文が表示される テキストRspec1のテスト' do
+      user = User.new
+      expect(user).not_to be_valid
+      # binding.irb
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+    it '名前が正しく入力されていないときエラー文が表示される テキストRspec3のテスト' do
+      user = User.new(name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include('を入力してください')
+    end
+    it '名前の文字数が255文字を超えるとエラー文が表示される' do
+      user = User.new(name: 'a' * 256)
+      user.valid?
+      # binding.irb
+      expect(user.errors[:name]).to include('は255文字以内で入力してください')
+    end
+  end
 end
