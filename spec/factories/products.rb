797b47032b2847bf98e213c60daa846d1031f3a7
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-08 19:20:18 +0900

    productのFactoryBotを3つ作成 #59

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,6 +1,15 @@
 FactoryBot.define do
+  
   factory :product do
-    # name { 'product1' }
-    # association :user
+    name { 'product1' }
+    association :user
+  end
+
+  factory :second_product, class: Product do
+    name { 'product2' }
+  end
+
+  factory :third_product, class: Product do
+    name { 'product3' }
   end
 end
