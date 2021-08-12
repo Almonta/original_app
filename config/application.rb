6daa537f9c8f7a4aab9a8fe486e76e4e7123350b
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-12 17:27:24 +0900

    資料の新規作成フォームのエラー件数表示を日本語化 #88

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MakerAwesome
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
