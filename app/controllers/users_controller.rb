8261380441c4b5ba6a383b5c46a9fa7c82c561c8
motohiro takagi <tkg.8.mthr@gmail.com>
2021-08-16 15:30:35 +0900

    マイページのドキュメントに編集削除リンク追加 #100

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    # @product = Product.where(id: params[:product_id]).first
+    # @customer = Customer.where(id: params[:customer_id]).first
     @products = Product.where(user_id: current_user.id)
     @customers = Customer.where(user_id: current_user.id)
     @product_documents = ProductDocument.where(user_id: current_user.id)
