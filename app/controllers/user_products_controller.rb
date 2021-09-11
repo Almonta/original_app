a0162809b5687f191f4a1a1bf7e7a55bd18a5e6a
motohiro takagi <tkg.8.mthr@gmail.com>
2021-09-11 18:32:39 +0900

    productとcustomerのメソッドがバッティングしていたため修正 #184

--- app/controllers/user_products_controller.rb
+++ app/controllers/user_products_controller.rb
@@ -1,11 +1,11 @@
 class UserProductsController < ApplicationController
   def create
     @product = Product.find(params[:product_id])
-    current_user.registered(@product)
+    current_user.product_registered(@product)
   end
 
   def destroy
     @product = current_user.registered_products.find(params[:id])
-    current_user.unregistered(@product)
+    current_user.product_unregistered(@product)
   end
 end
